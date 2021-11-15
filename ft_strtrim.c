a6371e58185e4b766f1a5881f1356329f6292b97
lcorinna <87826392+lcorinna@users.noreply.github.com>
2021-11-15 14:54:57 +0300

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: lcorinna <lcorinna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:20:59 by lcorinna          #+#    #+#             */
-/*   Updated: 2021/11/13 19:20:45 by lcorinna         ###   ########.fr       */
+/*   Updated: 2021/11/15 14:39:15 by lcorinna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,23 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*res;
-	int		i;
-	int		j;
-	int		res_len;
+	int		start;
+	int		len_res;
 
-	i = 0;
-	j = ft_strlen(s1);
-	res = (char *) set;
-	while (s1[i] && res != NULL)
-		res = ft_strchr(set, s1[i++]);
-	i--;
-	res = (char *) set;
-	while (j > 0 && res != NULL)
-		res = ft_strchr(set, s1[--j]);
-	res_len = j - i + 2;
-	if (j == 0)
-		res_len = 1;
-	res = (char *) malloc(sizeof(char) * res_len);
-	if (res == NULL)
-		return (res);
-	while (i <= j && res_len != 1)
-		*res++ = s1[i++];
-	*res = '\0';
-	j = 1 - res_len;
-	return (res + j);
+	start = 0;
+	res = (char *)s1;
+	while (*res && ft_strchr(set, *res) != NULL)
+	{
+		res++;
+		start++;
+	}
+	len_res = ft_strlen(res);
+	res += (len_res - 1);
+	while (len_res > 0 && ft_strchr(set, *res) != NULL)
+	{
+		res--;
+		len_res--;
+	}
+	res = ft_substr(s1, start, len_res);
+	return (res);
 }
